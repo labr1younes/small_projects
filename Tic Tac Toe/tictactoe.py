2117b70b33941d0ee5652ad2cbd5483a15cc7f51
Younes <younesetud@Gmail.Com>
2023-06-08 15:38:04 +0100

    working OK

--- Tic Tac Toe/tictactoe.py
+++ Tic Tac Toe/tictactoe.py
@@ -1,10 +1,12 @@
-feild = [ [1,2,3],
+import random
+
+field = [ [1,2,3],
           [4,5,6],
           [7,8,9] ]
 
-c_player = "X"
+c_player = random.choice(["X","O"])
 
-def print_feild(fe):
+def print_field(fe):
     print("{} | {} | {}".format(fe[0][0],fe[0][1],fe[0][2]))
     print("----------")
     print("{} | {} | {}".format(fe[1][0],fe[1][1],fe[1][2]))
@@ -17,20 +19,20 @@ def current_role(pla):
 def find(x):
     for i in range(3):
         for j in range(3):
-            if x == feild[i][j]:
+            if x == field[i][j]:
                 return i,j
 
 def play(position):
     global c_player
     x ,y = find(position)
     if c_player == "X":
-        feild[x][y] = "X"
+        field[x][y] = "X"
         c_player = "O"
     elif c_player == "O":
-        feild[x][y] = "O"
+        field[x][y] = "O"
         c_player = "X"
 
-def check(f):
+def check_winner(f):
     for i in range(3):
         if f[i][0] == f[i][1] == f[i][2]:
             return f[i][0]
@@ -45,21 +47,30 @@ def check(f):
     if f[0][2] == f[1][1] == f[2][0]:
         return f[0][2]
 
-    return "Tie"
+    return None
+
+def is_tie(f):
+    for i in f:
+        for j in i:
+            if isinstance(j, int):
+                return False
+    return True
 
 def main():
-    print_feild(feild)
-    plays = 0
+    print_field(field)
     while True:
+        print("It's the [{}] turn.".format(c_player))
         tmp = int(input("Pick a position : "))
+        
         play(tmp)
-        plays += 1
-        winner = check(feild)
-        print_feild(feild)
-        if winner == -1 and plays==9 :
-            print("Its a Tie , nom one wone the game")
+        winner = check_winner(field)
+        print_field(field)
+        if winner:
+            print("Player [{}] wins the game!".format(winner))
+            break
+        elif is_tie(field):
+            print("It's a tie. No one won the game.")
             break
         
-
 if __name__ == "__main__":
     main()

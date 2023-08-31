0d21f53a5fb3aee7814496822122f64ea519e1bc
Younes <younesetud@gmail.com>
2023-08-31 12:49:24 +0100

    added parser for CLI and is_valid_file_dir method

--- My Exif Tool/myexiftool.py
+++ My Exif Tool/myexiftool.py
@@ -1,10 +1,11 @@
 from PIL import Image
 from PIL.ExifTags import TAGS, GPSTAGS
 import os
+import argparse
 
 
 # Reading the image from a path
-def read_img(filepath) -> Image :
+def read_img(filepath) -> Image:
     myimg = Image.open(filepath)
     return myimg
 
@@ -19,7 +20,7 @@ def split_filepath(filepath):
 # Getting the most important data of the image
 def get_basic_data(filepath) -> dict:
     image = read_img(filepath)
-    imagename = split_filepath(filepath)
+    imagename, t, p = split_filepath(filepath)
     basic_data = {
         "Filename": imagename,
         "Image Size": image.size,
@@ -28,7 +29,6 @@ def get_basic_data(filepath) -> dict:
         "Image Format": image.format,
         "Image Mode": image.mode
     }
-
     return basic_data
 
 
@@ -66,7 +66,7 @@ def print_exif_data(filepath) -> None:
 
 
 # Removing the Exif data from the image
-def rmv_exif_data(filepath :str) -> None:
+def rmv_exif_data(filepath: str) -> None:
     image = read_img(filepath)
     imagename, imagetype, imagepath = split_filepath(filepath)
     data = list(image.getdata())
@@ -88,29 +88,37 @@ def add_mult_exif(newexif, exif_dic: dict) -> None:
         add_exif(newexif, tag, val)
 
 
+# Preparing the parser
+def prs():
+    parser = argparse.ArgumentParser(description='Process Exif Data of Images')
+    parser.add_argument("directory", help="specify the directory that contains the images ")
+    parser.add_argument("-sh", "--show", required=False, help="Show the Exif data of the image ")
+
+    arg = parser.parse_args()
+    return arg
+
+
+# Checking if the user's file path input is valid
+def is_valid_file_dir(path):
+    if not os.path.isfile(path):
+        print("Error: File does not exist.")
+        return False
+    elif not path.lower().endswith(".jpg"):
+        print("Error: Invalid file format. Only (*.jpg) files are supported.")
+        return False
+    else:
+        return True
+
+
+# Our main function
+def exf_tool(arg):
+    file_path = arg.directory
+    if is_valid_file_dir(file_path):
+        print_basic_data(file_path)
+    else:
+        print("\n Process Failed \n")
+
+
 if __name__ == "__main__":
-    image = Image.open('Canon_DIGITAL_IXUS_400.jpg')
-
-    # for i in TAGS:
-    #     data = TAGS.get(i)
-    #     # decode bytes
-    #     if isinstance(data, bytes):
-    #         data = data.decode()
-    #     print(f"{data:25}: {i}")
-
-    exif = image.getexif()
-    exifnew = {
-        271: 'hello',  # Date and time of the photo.
-        9595: 'this',  # Camera make and model.
-        1468: 'is  ',  # Camera make and model.
-        2033: 'myexiftool',  # Camera make and model.
-    }
-
-    add_mult_exif(exif, exifnew)
-    # add_exif(exif, 2020, "i dont know")
-
-    # Save the image with the updated EXIF data.
-    image.save('image_with_exif.jpg', exif=exif)
-    print_exif_data('image_with_exif.jpg')
-
-
+    args = prs()
+    exf_tool(args)

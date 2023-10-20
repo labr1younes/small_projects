d8dd65703c00673c3e0474d95d664bfc9a5ba791
Younes <younesetud@gmail.com>
2023-10-20 23:21:33 +0100

    fixed KeyError bug

--- FB_Automater/fbauto.py
+++ FB_Automater/fbauto.py
@@ -5,28 +5,41 @@ from dotenv import load_dotenv
 
 def filtr(responsejson):
     for result in responsejson["results"]:
-        try:
-            if result["language"] not in ["English", "Arabic"]:
-                continue
-            else:
-                print("category = ", result["category"])
-                # print("catslug = ", result["catslug"])
-                # print("date = ", result["date"])
-                print("id = ", result["id"])
-                # print("image = ", result["image"])
-                # print("isexpired = ", result["isexpired"])
-                # print("language = ", result["language"])
-                # print("name = ", result["name"])
-                # print("shoer_description = ", result["shoer_description"])
-                # print("store = ", result["store"])
-                # print("subcategory = ", result["subcategory"])
-                # print("subcatslug = ", result["subcatslug"])
-                # print("type = ", result["type"])
-                # print("url = ", result["url"])
-                print("---------------------------------------------")
-        except Exception as error:
-            # handle the exception
-            print("An exception occurred:", error)
+        if result["type"] == "Affiliate":
+            continue
+
+        category = result.get("category", None)
+        catslug = result.get("catslug", None)
+        date = result.get("date", None)
+        idd = result.get("id", None)
+        image = result.get("image", None)
+        isexpired = result.get("isexpired", None)
+        language = result.get("language", None)
+        shoer_description = result.get("shoer_description", None)
+        store = result.get("store", None)
+        subcategory = result.get("subcategory", None)
+        subcatslug = result.get("subcatslug", None)
+        typee = result.get("type", None)
+        urll = result.get("url", None)
+
+        if language not in ["English", "Arabic"]:
+            continue
+        else:
+            print("category = ", category)
+            print("catslug = ", catslug)
+            print("date = ", date)
+            print("id = ", idd)
+            print("image = ", image)
+            print("isexpired = ", isexpired)
+            print("language = ", language)
+            print("name = ", result["name"])
+            print("shoer_description = ", shoer_description)
+            print("store = ", store)
+            print("subcategory = ", subcategory)
+            print("subcatslug = ", subcatslug)
+            print("type = ", typee)
+            print("url = ", urll)
+            print("---------------------------------------------")
 
 
 if __name__ == "__main__":

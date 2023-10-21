f60f2550efa69a2cf9ff6514661f1ce7651f88c0
Younes <younesetud@gmail.com>
2023-10-21 23:25:15 +0100

    added print_data()

--- FB_Automater/fbauto.py
+++ FB_Automater/fbauto.py
@@ -5,9 +5,12 @@ import mysql.connector
 
 
 def filtr(responsejson):
+    filtred_data = []
     for result in responsejson["results"]:
         if result["type"] == "Affiliate":
             continue
+
+        tmp_result = []
 
         category = result.get("category", None)
         # catslug = result.get("catslug", None)
@@ -16,6 +19,7 @@ def filtr(responsejson):
         image = result.get("image", None)
         # isexpired = result.get("isexpired", None)
         language = result.get("language", None)
+        name = result.get("name", None)
         shoer_description = result.get("shoer_description", None)
         # store = result.get("store", None)
         subcategory = result.get("subcategory", None)
@@ -26,32 +30,34 @@ def filtr(responsejson):
         if language not in ["English", "Arabic"]:
             continue
         else:
-            print("category = ", category)
-            # print("catslug = ", catslug)
-            # print("date = ", date)
-            # print("id = ", idd)
-            print("image = ", image)
-            # print("isexpired = ", isexpired)
-            print("language = ", language)
-            print("name = ", result["name"])
-            print("shoer_description = ", shoer_description)
-            # print("store = ", store)
-            print("subcategory = ", subcategory)
-            # print("subcatslug = ", subcatslug)
-            print("type = ", typee)
-            print("url = ", urll)
-            print("---------------------------------------------")
+            tmp_result = [category, image, language, name, shoer_description, subcategory, typee, urll]
+            print("--------------------------------------------- ADDED")
+        filtred_data.append(tmp_result)
+    return filtred_data
+
+
+def print_data(data):
+    for cours in data:
+        print("category = ", cours[0])
+        print("image = ", cours[1])
+        print("language = ", cours[2])
+        print("name = ", cours[3])
+        print("shoer_description = ", cours[4])
+        print("subcategory = ", cours[5])
+        print("type = ", cours[6])
+        print("url = ", cours[7])
+        print("---------------------------------------------")
+    print("--- Done ---")
 
 
 if __name__ == "__main__":
     load_dotenv()
     url = os.getenv('URL')
 
-    print("Hello world! \n")
-
     response = requests.get(url)
     print(len(response.json()["results"]))
-    filtr(response.json())
+    tmpdata = filtr(response.json())
+    print_data(tmpdata)
 
     mydb = mysql.connector.connect(
         host=os.getenv('HOST'),
